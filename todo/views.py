9a2bfcc2c78cf826a2b4480d7f01a8ab898d0911
Christoph33one <cjbrowning23@hotmail.com>
2022-12-29 11:46:21 +0000

    setting up a html file (todo_list.html), linking file to render and display html in the browser

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello!")
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
+    #  passsing the html file in the render (import) to django,
+    # to display in the browser
