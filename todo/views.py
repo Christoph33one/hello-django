218c412c312a5b27fddfda30ec75e1ee80a316fb
Christoph33one <cjbrowning23@hotmail.com>
2023-01-04 16:21:55 +0000

    have errors when adding a function to test test_get_edit_item function.

--- todo/views.py
+++ todo/views.py
@@ -37,7 +37,7 @@ def edit_item(request, item_id):
             return redirect('get_todo_list')
     form = ItemForm(instance=item)
     context = {
-         'form': form
+        'form': form
     }
     return render(request, 'todo/edit_item.html', context)
 
@@ -46,4 +46,10 @@ def toggle_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.done = not item.done
     item.save()
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
     return redirect('get_todo_list')
